87d5c819a326e39793be9fa1e37894fd767b87db
Kristy Guo <krisxxg@gmail.com>
2023-02-24 06:31:28 -0800

    add new project

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,90 +1,129 @@
 import React from "react";
-import './experience.css';
-import {ImHtmlFive} from 'react-icons/im';
-import {FaCss3}  from 'react-icons/fa';
-import {IoLogoJavascript} from 'react-icons/io';
-import {FaReact} from 'react-icons/fa';
-import {BsBootstrapFill} from 'react-icons/bs';
+import "./experience.css";
+import { ImHtmlFive } from "react-icons/im";
+import { FaCss3 } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaReact } from "react-icons/fa";
+import { BsBootstrapFill } from "react-icons/bs";
 
-import {GrNode} from 'react-icons/gr';
-import {SiExpress} from 'react-icons/si';
-import {SiMongodb} from  'react-icons/si';
-import {GrMysql}  from 'react-icons/gr';
-import {SiGraphql} from 'react-icons/si';
-
+import { GrNode } from "react-icons/gr";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+import { SiGraphql } from "react-icons/si";
 
 const Experience = () => {
-    return (
-        <section id="experience">
-            <h5>Technologies that I work with the most</h5>
-            <h2>My skills</h2>
+  return (
+    <section id="experience">
+      <h5>Technologies that I work with the most</h5>
+      <h2>My skills</h2>
 
-            <div className="container experience__container">
-                <div className="experience__frontend">
-                    <h3>Frontend Development</h3>
-                    <div className="experience__content">
-                        <article className="experience__details">
-                            <ImHtmlFive className="experience__details-icon"/>
-                            <h4>HTML</h4>
-                        </article>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
 
-                        <article className="experience__details">
-                            <FaCss3 className="experience__details-icon"/>
-                            <h4>CSS</h4>
-                        </article>
+          <div className="experience__content">
+            <h4>Next.js</h4>
+            <article className="experience__details">
+              <FaReact className="experience__details-icon" />
+              <h4>React.js</h4>
+            </article>
+            <article className="experience__details">
+              <h4>TypeScript</h4>
+            </article>
 
-                        <article className="experience__details">
-                            <IoLogoJavascript className="experience__details-icon"/>
-                            <h4>JavaScript</h4>
-                        </article>
+            <article className="experience__details">
+              <IoLogoJavascript className="experience__details-icon" />
+              <h4>JavaScript</h4>
+            </article>
+            <article className="experience__details">
+              <h4>TailwindCSS</h4>
+            </article>
 
-                        <article className="experience__details">
-                            <FaReact className="experience__details-icon"/>
-                            <h4>React</h4>
-                        </article>
+            <article className="experience__details">
+              <BsBootstrapFill className="experience__details-icon" />
+              <h4>Bootstrap</h4>
+            </article>
+          </div>
+        </div>
 
-                        <article className="experience__details">
-                            <BsBootstrapFill className="experience__details-icon"/>
-                            <h4>Bootstrap</h4>
-                        </article>
-                    </div>
-                </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <GrNode className="experience__details-icon" />
+              <h4>Node JS</h4>
+            </article>
 
-                <div className="experience__backend">
-                    <h3>Backend Development</h3>
-                    <div className="experience__content">
+            <article className="experience__details">
+              <SiExpress className="experience__details-icon" />
+              <h4>Express JS</h4>
+            </article>
 
-                        <article className="experience__details">
-                            <GrNode className="experience__details-icon"/>
-                            <h4>Node JS</h4>
-                        </article>
+            <article className="experience__details">
+              <SiMongodb className="experience__details-icon" />
+              <h4>MongoDB</h4>
+            </article>
 
-                        <article className="experience__details">
-                            <SiExpress className="experience__details-icon"/>
-                            <h4>Express JS</h4>
-                        </article>
+            <article className="experience__details">
+              <GrMysql className="experience__details-icon" />
+              <h4>Mysql</h4>
+            </article>
 
-                        <article className="experience__details">
-                            <SiMongodb className="experience__details-icon"/>
-                            <h4>MongoDB</h4>
-                        </article>
+            <article className="experience__details">
+              <SiGraphql className="experience__details-icon" />
+              <h4>GraphQL</h4>
+            </article>
+            <article className="experience__details">
+              <SiGraphql className="experience__details-icon" />
+              <h4>Prisma</h4>
+            </article>
+            <article className="experience__details">
+              <SiGraphql className="experience__details-icon" />
+              <h4>tRPC</h4>
+            </article>
+          </div>
+        </div>
+        <div>
+          <h3>Other Technologies</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <GrNode className="experience__details-icon" />
+              <h4>GitHub</h4>
+            </article>
 
-                        <article className="experience__details">
-                            <GrMysql className="experience__details-icon"/>
-                            <h4>Mysql</h4>
-                        </article>
+            <article className="experience__details">
+              <GrNode className="experience__details-icon" />
+              <h4>Vercel</h4>
+            </article>
 
-                        <article className="experience__details">
-                            <SiGraphql className="experience__details-icon"/>
-                            <h4>GraphQL</h4>
-                        </article>
+            <article className="experience__details">
+              <GrNode className="experience__details-icon" />
+              <h4>Heroku</h4>
+            </article>
 
-                    </div>
-                </div>
+            <article className="experience__details">
+              <GrNode className="experience__details-icon" />
+              <h4>AWS</h4>
+            </article>
 
-            </div>
-        </section>
-    )
-}
+            <article className="experience__details">
+              <GrNode className="experience__details-icon" />
+              <h4>Figma</h4>
+            </article>
+            <article className="experience__details">
+              <GrNode className="experience__details-icon" />
+              <h4>Firebase</h4>
+            </article>
+            <article className="experience__details">
+              <GrNode className="experience__details-icon" />
+              <h4>Sanity.io</h4>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Experience;
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Resume from '../../assets/resumeKg7.7.22.pdf'
-const CTA = ()=>{
-    return(
-        <div className="cta">
-            <a href={Resume} download className="btn">Download Resume</a>
-            <a href="#contact" className="btn btn-primary">Let's Talk</a>
-        </div>
-    )
-}
+import Resume from "../../assets/resume_swe.pdf";
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a href={Resume} download className="btn">
+        Download Resume
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
+    </div>
+  );
+};
 
 export default CTA;
